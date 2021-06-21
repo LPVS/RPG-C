268e10cf780a54a4441018b29fc4f2638e9ee3c4
Lucas <lucaspvsantos123@gmail.com>
2021-06-21 09:33:39 -0300

    Teste

---
+++ Classe/Teste.cpp
@@ -0,0 +1,15 @@
+class Personagem {
+    private:
+        /* data */
+    public:
+        Personagem(/* args */);
+        ~Personagem();
+};
+
+Personagem::Personagem(/* args */) {
+
+}
+
+Personagem::~Personagem() {
+
+}
