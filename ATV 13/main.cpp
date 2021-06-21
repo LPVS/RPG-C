7d77fee0c075ed337ae463d873e40fa3295eecc3
Lucas <lucaspvsantos123@gmail.com>
2021-06-21 10:42:08 -0300

    .

--- ATV 13/main.cpp
+++ ATV 13/main.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 using namespace std;
 
-Personagem::Personagem (int vida, int mana, int forca, int magia, int resFisico, int resMagia, int agilidade) {
+Personagem::Personagem(int vida, int mana, int forca, int magia, int resFisico, int resMagia, int agilidade)
+{
     this->vida = vida;
     this->mana = mana;
     this->forca = forca;
@@ -12,13 +13,59 @@ Personagem::Personagem (int vida, int mana, int forca, int magia, int resFisico,
     this->agilidade = agilidade;
 }
 
-void Personagem::imprimeDados(){
+void Personagem::imprimeDados()
+{
     cout << vida << " - " << mana << " - " << forca << " - " << magia << " - ";
     cout << resFisico << " - " << resMagia << " - " << agilidade;
 }
 
-int main(){
-    Personagem *guerreiro = new Personagem (4000,30,100,20,80,20,20);
-    guerreiro -> imprimeDados();
+int main()
+{
+    int op[2], stop;
+    for (int i = 1; i <= 2; i++)
+    {
+        stop = 0;
+        do
+        {
+            cout << "Jogador " << i << "\nEscolha seu personagem: \n 1-Guerreiro \n 2-Ladrao \n 3-Mago \n 4-Paladino \n --";
+            scanf("%d", &op[i]);
+            getchar();
+            switch (op[i])
+            {
+            case 1:
+                
+                break;
+            case 2:
+                /* code */
+                break;
+            case 3:
+                /* code */
+                break;
+            case 4:
+                /* code */
+                break;
+            case 5:
+                /* code */
+                break;
+            case 6:
+                /* code */
+                break;
+            case 7:
+                /* code */
+                break;
+            case 8:
+                /* code */
+                break;
+            default:
+                cout << "Opcao invalida, tente novamente\n";
+                break;
+            }
+        } while (stop = 0);
+    }
+    /*
+    //Definir personagens
+    Personagem *guerreiro = new Personagem(4000, 30, 100, 20, 80, 20, 20);
+    guerreiro->imprimeDados();
     return 0;
+    */
 }
