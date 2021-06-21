e196059efbd0ac1203173ad128003f2617fce84d
Lucas <lucaspvsantos123@gmail.com>
2021-06-21 11:23:47 -0300

    Mais classe

--- ATV 13/main.cpp
+++ ATV 13/main.cpp
@@ -21,51 +21,64 @@ void Personagem::imprimeDados()
 
 int main()
 {
-    int op[2], stop;
+    //Definir personagens
+    Personagem *guerreiro = new Personagem(4000, 30, 100, 20, 80, 20, 20);
+    Personagem *ladrao = new Personagem(2800, 50, 50, 30, 40, 50, 80);
+    Personagem *mago = new Personagem(2500, 100, 40, 100, 30, 80, 40);
+    Personagem *paladino = new Personagem(3200, 80 ,60, 50, 60, 60, 60);
+    Personagem *animal = new Personagem(3200,30,80,20,80,20,50);
+    Personagem *troll = new Personagem(2800,20,100,20,80,20,20);
+    Personagem *dragao = new Personagem(3000,40,100,20,50,50,30);
+    Personagem *zumbi = new Personagem(2500,20,40,20,40,80,50);
+
+    //Definir variáveis
+    int op, stop;
+    int *teste;
+
+    //Escolher personagens
     for (int i = 1; i <= 2; i++)
     {
-        stop = 0;
         do
         {
+            stop = 1;
             cout << "Jogador " << i << "\nEscolha seu personagem: \n 1-Guerreiro \n 2-Ladrao \n 3-Mago \n 4-Paladino \n --";
-            scanf("%d", &op[i]);
+            scanf("%d", &op);
             getchar();
-            switch (op[i])
+            cout << op << "\n";
+            switch (op)
             {
             case 1:
                 
                 break;
             case 2:
-                /* code */
+                
                 break;
             case 3:
-                /* code */
+                
                 break;
             case 4:
-                /* code */
+                
                 break;
+            /*
             case 5:
-                /* code */
+                
                 break;
             case 6:
-                /* code */
+                
                 break;
             case 7:
-                /* code */
+                
                 break;
             case 8:
-                /* code */
+                
                 break;
+            */
             default:
                 cout << "Opcao invalida, tente novamente\n";
+                stop = 0;
                 break;
             }
-        } while (stop = 0);
+        } while (stop == 0);
     }
-    /*
-    //Definir personagens
-    Personagem *guerreiro = new Personagem(4000, 30, 100, 20, 80, 20, 20);
-    guerreiro->imprimeDados();
     return 0;
-    */
 }
