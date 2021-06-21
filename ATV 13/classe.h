b137bba325790fbdcb6c2b885192b91ae0a75837
Lucas <lucaspvsantos123@gmail.com>
2021-06-21 11:29:13 -0300

    Arma e Magia

--- ATV 13/classe.h
+++ ATV 13/classe.h
@@ -5,7 +5,7 @@ using namespace std;
 
 class Personagem
 {
-private:
+protected:
     int vida;
     int mana;
     int forca;
@@ -19,4 +19,26 @@ public:
     Personagem(int vida, int mana, int forca, int magia, int resFisico, int resMagia, int agilidade);
     void imprimeDados();
     ~Personagem();
+};
+
+class Arma
+{
+protected:
+    int id;
+    int danoMin;
+    int danoMax;
+public:
+    Arma(int id, int danoMin, int danoMax);
+    ~Arma();
+};
+
+class Magia
+{
+private:
+    int id;
+    bool dano;
+    int gasto;
+public:
+    Magia(int id, bool dano, int gasto);
+    ~Magia();
 };
